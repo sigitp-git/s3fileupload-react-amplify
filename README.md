65161f7378ea6435a4b207beb6d1427d33977bd6
sigitp-git <sigitp@amazon.com>
2020-06-05 22:22:46 -0500

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+## Simple S3 Upload with React and AWS Amplify
+
+Users need to Sign-Up with Email Address, Phone Number
+Email Address must be valid, Cognito will send confirmation number to the Email Address
+Once confirmation number verified, user can start Siging-In
+
+Sing In using Email Address as the user-name
+
+Accepts images and videos
+
+## 💫 Deploy
+
+[![amplifybutton](https://oneclick.amplifyapp.com/button.svg)](https://console.aws.amazon.com/amplify/home#/deploy?repo=https://github.com/sigitp-git/s3fileupload-react-amplify)
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import Amplify, { Storage } from 'aws-amplify'
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { MdSend } from 'react-icons/md'
+import awsConfig from './aws-exports'
+Amplify.configure(awsConfig)
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('')
+  const [file, setFile] = useState('')
+  const [response, setResponse] = useState('')
+
+  const onChange = (e) => {
+    e.preventDefault()
+    setFile(e.target.files[0])
+    setName(e.target.files[0].name)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    Storage.put(name, file, {
+      contentType: file.type,
+    })
+      .then((result) => {
+        console.log(result)
+        setResponse(`Success uploading file: ${name}!`)
+      })
+      .catch((err) => {
+        console.log(err)
+        setResponse(`Can't upload file: ${err}`)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AmplifyAuthenticator>
+      <div className='header'>
+        <h2>
+          <a href='/'>TCS Team Video Uploader</a>
+        </h2>
+      </div>
+      <div className='video-uploader'>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <p>
+            <label className='select-label'>Select video: </label>
+          </p>
+          <p>
+            <input
+              className='video-input'
+              type='file'
+              id='video'
+              accept='image/*, video/*'
+              onChange={(e) => onChange(e)}
+            />
+          </p>
+          <button type='submit' className='btn'>
+            Submit <MdSend className='btn-icon' />
+          </button>
+        </form>
+      </div>
+
+      {response && <div className='upload-status'>{response}</div>}
+
+      <div className='sign-out'>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
+  )
 }
 
-export default App;
+export default App
